0b1c454291732529ba3122869e8b33db05dc9d90
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-04-25 12:42:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 console.log("hello world")
